d96947ded971cd9e5ea3327720d85bee9f8a1810
Matin De Wet <matindewet116@gmail.com>
2020-09-08 14:57:24 +0200

    more saving

--- PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/TextFileDataHandeler.cs
+++ PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/TextFileDataHandeler.cs
@@ -13,7 +13,7 @@ namespace PRG282_Project_LijaniVWDV_JohannesDW.DataHandlers
         private string filepath = @"Resources\TextFiles\MissionReport.txt";
         /*
             the format of the file should be as follows
-            (targetes identified)^(targeted attacked)^(targeted locaiton)^(target damage delt)^(mission success persentage)^(mission time)^(Plane used)^
+            (targetes identified)^(targeted attacked)^(targeted locaiton)^(target damage delt)^(mission success persentage)^(mission time)^(Plane used)^()
          */
 
 
