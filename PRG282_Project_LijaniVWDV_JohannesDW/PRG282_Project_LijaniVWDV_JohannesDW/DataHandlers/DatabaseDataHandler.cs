28182fc58f95a09c2d7b4e40a03dc2b92ae6fb18
Lijani <lijanivwdv@gmail.com>
2020-09-08 19:15:08 +0200

    Fix

--- PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/DatabaseDataHandler.cs
+++ PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/DatabaseDataHandler.cs
@@ -3,13 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
 
 namespace PRG282_Project_LijaniVWDV_JohannesDW.DataHandlers
 {
     class DatabaseDataHandler
     {
-
         /*
             to connect to the database making use of the sql server managment studio use the following:
             server name:    militarysim.database.windows.net
@@ -17,11 +15,8 @@ namespace PRG282_Project_LijaniVWDV_JohannesDW.DataHandlers
             login:          MatinLijani
             Password:       ThereIs@Horse@1234
          */
-<<<<<<< HEAD:PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/DatabaseDataHandler.cs
 
         private string connectionString = @"Server=tcp:militarysim.database.windows.net,1433;Initial Catalog=MilitarySimDataDase;Persist Security Info=False;User ID=MatinLijani;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-=======
-        private string connectionString = @"Server=tcp:militarysim.database.windows.net,1433;Initial Catalog=MilitarySimDataDase;Persist Security Info=False;User ID=MatinLijani;Password=ThereIs@Horse@1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
->>>>>>> 3d6abac0655466ec3829dca10fed17465e6d9ca7:PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/DataBaseDataHandeler.cs
+
     }
 }
