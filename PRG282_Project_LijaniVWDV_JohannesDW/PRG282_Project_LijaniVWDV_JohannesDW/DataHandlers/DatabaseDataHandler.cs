905af255d324e6e4de84ad953f318f3cd20c5ef7
Matin De Wet <matindewet116@gmail.com>
2020-09-23 12:45:56 +0200

    fixed the database class
    removed litrally everything ;)

--- PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/DatabaseDataHandler.cs
+++ PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/DataHandlers/DatabaseDataHandler.cs
@@ -8,15 +8,6 @@ namespace PRG282_Project_LijaniVWDV_JohannesDW.DataHandlers
 {
     class DatabaseDataHandler
     {
-        /*
-            to connect to the database making use of the sql server managment studio use the following:
-            server name:    militarysim.database.windows.net
-            authentication: sql server authentication
-            login:          MatinLijani
-            Password:       ThereIs@Horse@1234
-         */
-
-        private string connectionString = @"Server=tcp:militarysim.database.windows.net,1433;Initial Catalog=MilitarySimDataDase;Persist Security Info=False;User ID=MatinLijani;Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-
+        
     }
 }
