315fe75dc322a3b5f29c9ba56d88b57466b2cc5e
Lijani <lijanivwdv@gmail.com>
2020-10-02 16:46:59 +0200

    QuickRecap

--- PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/Program.cs
+++ PRG282_Project_LijaniVWDV_JohannesDW/PRG282_Project_LijaniVWDV_JohannesDW/Program.cs
@@ -17,7 +17,7 @@ namespace PRG282_Project_LijaniVWDV_JohannesDW
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmIntro());
+            Application.Run(new MainSimScreen());
         }
     }
 }
